d96e832ef7e5bd3fb1a4a9c64468333bf22aa997
David <dmackeyward@gmail.com>
2026-08-10 16:33:41 +1200

    install Markdown for Svelte

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
+import { mdsvex } from 'mdsvex';
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    sveltekit({
+      preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
+      extensions: ['.svelte', '.svx', '.md']
+    })
+  ],
   resolve: {
-    alias: {
-      $lib: fileURLToPath(new URL('./src/lib', import.meta.url))
-    }
+    alias: { $lib: fileURLToPath(new URL('./src/lib', import.meta.url)) }
   }
 });
